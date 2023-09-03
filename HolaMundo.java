fb79bd09c50d49c0c9230c5dc4f07b864c796ea9
JesusTapiaMartin <jesustapiamartin@gmail.com>
2023-09-03 20:23:57 -0300

    Commit con nombre y apellidos

--- HolaMundo.java
+++
@@ -1,8 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println(" Hola Mundo!!!! ");
-        System.out.println(" Esta es la evidencia 02 ");
-        System.out.println(" Adiós... ");
-        // Jesús Tapia
-    }
-}
---
+++ HolaMundoo.java
@@ -0,0 +1,8 @@
+public class HolaMundoo {
+    public static void main(String[] args) {
+        System.out.println(" Hola Mundo!!!! ");
+        System.out.println(" Esta es la evidencia 02 ");
+        System.out.println(" Adiós... ");
+        // Jesús Tapia
+    }
+}
