d6250ab2b380a3985524cb456a894ab9a59507dd
Jesús Tapia Martin <142464450+JesusTapiaMartin@users.noreply.github.com>
2023-09-03 20:05:16 -0300

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println(" Hola Mundo!!!! ");
+        System.out.println(" Esta es la evidencia 02 ");
+        System.out.println(" Adiós... ");
+    }
+}
